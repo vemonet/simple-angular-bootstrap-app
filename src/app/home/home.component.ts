69dd6a6beb27a36c0eeb5072f00e241276baf071
vemonet <vincent.emonet@gmail.com>
2019-04-28 19:14:33 +0200

    add HttpClient and get data onInit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,16 +8,19 @@ import { DataService } from '../data.service';
 })
 export class HomeComponent implements OnInit {
 
-  h1Style: boolean = false;
+  users: Object;
 
   constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.data.getUsers().subscribe(data => {
+      this.users = data
+      console.log(this.users);
+    } );
   }
 
   firstClick() {
-    this.h1Style = true;
-    this.data.firstClick();
+    //this.data.firstClick();
     console.log('clicked');
   }
 
